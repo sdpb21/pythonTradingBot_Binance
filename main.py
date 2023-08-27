90e4d76da957295d8bea5a08cd2d014682b54ae3
sdpb21 <jytprf@gmail.com>
2023-08-27 14:37:12 -0300

    First buy out of the while loop

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from pprint import pprint
 import config
 
 client = Spot() # my first spot object
+buy = False
+sell = False
+price = 0.0
 
 print("client.time(): ",client.time())
 
 #pprint(client.klines("BTCTUSD","15m",limit=1))
+
+price = float(client.ticker_price("BTCTUSD").get('price'))
+print("buy BTC at: ",price)
+buy = True
 
 while True:
     pprint(float(client.ticker_price("BTCTUSD").get('price')))
